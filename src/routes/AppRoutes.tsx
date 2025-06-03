1a68ba614eeb7fc0c36c7d4a6fb24e70fba59dd6
Rishabh <rishabh.sharma3@appinventiv.com>
2025-06-03 16:12:51 +0530

    Updated Landing Page code

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -11,7 +11,7 @@ import AdminLayout from "../layouts/AdminLayout";
 import Dashboard from "../features/admin/dashboard/Dashboard";
 import Register from "../features/auth/pages/Register";
 import HomePage from "../features/auth/pages/HomePage";
-import RestaurantManagerLanding from "../features/restrauntManager/RestaurantManagerLanding";
+import RestaurantManagerLanding from "../features/restrauntManager/LandingPage/RestaurantManagerLanding";
 
 const AppRoutes = () => {
   const router = createBrowserRouter([
