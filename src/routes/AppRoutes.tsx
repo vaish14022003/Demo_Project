9cefd021970cc2170994dd53e118424cfe983f9b
Rishabh <rishabh.sharma3@appinventiv.com>
2025-06-10 12:05:41 +0530

    Handled minor changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,33 +4,27 @@ import {
   Outlet,
 } from "react-router-dom";
 
-// Layouts
 import AppLayout from "../layouts/AppLayout";
 import AuthLayout from "../layouts/AuthLayout";
 import AdminLayout from "../layouts/AdminLayout";
 import Layout from "../features/restaurants/components/layout/Layout";
 
-// Restaurant Manager Pages
 import RestaurantManagerLanding from "../features/restrauntManager/LandingPage/RestaurantManagerLanding";
 import RestaurantForm from "../features/restrauntManager/InfoPage/RestaurantForm";
 import ReferralForm from "../features/restrauntManager/InfoPage/ReferralForm";
 
-// Auth Pages
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import HomePage from "../features/auth/pages/HomePage";
 import ErrorPage from "../features/auth/components/ErrorPage";
 
-// Customer Pages
 import PlaceOrderPage from "../features/customer/orderPlacement/PlaceOrderPage";
 import OrderStatusPage from "../features/customer/orderStatus/OrderStatusPage";
 import OrderHistoryPage from "../features/customer/orderHistory/OrderHistoryPage";
 
-// Admin Pages
 import OrderManagmentPage from "../features/admin/orders/OrderManagmentPage";
 import Dashboard from "../features/admin/dashboard/Dashboard";
 
-// Restaurant Dashboard Pages
 import Home from "../features/restaurants/pages/Home";
 import Menu from "../features/restaurants/pages/Menu";
 import Orders from "../features/restaurants/pages/Orders";
@@ -68,14 +62,12 @@ const router = createBrowserRouter([
     ],
   },
 
-  // ✅ Restaurant Manager Flow: No layout
   { path: "/restaurant-manager", element: <RestaurantManagerLanding /> },
   { path: "/restaurant-manager/register", element: <Register /> },
   { path: "/restaurant-manager/info", element: <RestaurantForm /> },
   { path: "/restaurant-manager/refer-form", element: <ReferralForm /> },
   { path: "/restaurant-manager/login", element: <Login /> }, // manager login
 
-  // ✅ Restaurant Dashboard (after login)
   {
     path: "/restaurant",
     element: (
